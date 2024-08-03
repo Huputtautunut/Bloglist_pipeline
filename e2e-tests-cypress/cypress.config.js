be2adeac54176c89782b139cf3d915de8bda78c5
Huputtautunut <jonsku022@gmailcom>
2024-08-03 21:12:27 +0300

    changess to cypress

--- e2e-tests-cypress/cypress.config.js
+++ e2e-tests-cypress/cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      baseUrl: 'http://localhost:5173'
     },
+    baseUrl: 'http://localhost:5173', // Correctly place the baseUrl here
   },
 });
